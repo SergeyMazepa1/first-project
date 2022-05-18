6e870201a48722eac23c7dea9304b77a08f1509f
SergeyMazepa <crap437460@yandex.ru>
2022-05-18 09:46:39 +0200

    Был изменен файл Practikum.java

--- Praktikum.java
+++ Praktikum.java
@@ -2,5 +2,11 @@ class Praktikum{
 public static void main(String[] args()){
 	System.out.println("Привет Сергей!");
 	System.out.println("Привет красавчик!");
+	System.out.println("Привет сергей");
+	int a = 10;
+	int b = 20;
+	if (a < b){
+	System.out.println("Все можешь идти гулять");
+	}
 }
 }
